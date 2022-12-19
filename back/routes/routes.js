34289178c0c3327c81180b82d0f737a77326d9e6
Perna <aber00fort@gmail.com>
2022-12-19 11:28:12 -0300

    Esquema de horario feito

--- back/routes/routes.js
+++ back/routes/routes.js
@@ -1,5 +1,5 @@
 const {Key, Teacher, TeacherKey} = require('../models/model');
-const {token, setToken} = require('../utils/utils')
+const {token, setToken, dealLock} = require('../utils/utils')
 const express = require('express');
 const router = express.Router();
 
@@ -11,8 +11,7 @@ router.post('/login', async (req, res)=>{
             console.log(data[0]);
             const token = await setToken(req.body.name, req.body.password);
             console.log(token);
-            if(data[0].name == "Admin")  return res.status(202).json({token: token, fechadura: await Key.find({}, {lockId: 1, _id:0})});
-            else return res.status(202).json({token: token, fechadura: await TeacherKey.find({teacherName: data[0].name}, {lockFID: 1, _id: 0, schedule: 1})});
+            return res.status(202).json({token: token, fechadura: await dealLock(data[0].name)});
         }
         else return  res.status(401).json("Senha incorreta");
     }
@@ -26,6 +25,53 @@ router.post('/register', async (req, res)=>{
         const data = await Teacher.aggregate([{$match:{name: name}}]);
         if(data.length == 0){
             if(req.body.name == "Admin"){
+                var relation
+                var docs = [];
+                var aux = 1;
+                for(i=101; i<121; i++){
+                    relation = new TeacherKey({
+                        lockFID:"A"+i, 
+                        teacherName: "Admin",
+                        schedule: ["00:00","23:59"],
+                        accepted: true
+                    })
+                    docs.push(relation);
+                    aux++;
+                }   
+                for(i=201; i<211; i++){
+                    relation = new TeacherKey({
+                        lockFID:"A"+i, 
+                        teacherName: "Admin",
+                        schedule: ["00:00","23:59"],
+                        accepted: true
+                    });
+                    docs.push(relation);
+                    aux++;
+                }
+                for(i=301; i<321; i++){
+                    relation = new TeacherKey({
+                        lockFID:"A"+i, 
+                        teacherName: "Admin",
+                        schedule: ["00:00","23:59"],
+                        accepted: true
+                    });
+                    docs.push(relation);
+                    aux++;
+                }
+                for(i=101; i<121; i++){
+                    relation = new TeacherKey({
+                        lockFID:"C"+i, 
+                        teacherName: "Admin",
+                        schedule: ["00:00","23:59"],
+                        accepted: true
+                    })
+                    docs.push(relation);
+                    aux++;
+                }
+                const keySet = await TeacherKey.insertMany(docs);
+                if(!keySet) {
+                    return res.status(400).json({})
+                }
                 const user = new Teacher({
                     name: name,
                     password: req.body.password
@@ -33,34 +79,36 @@ router.post('/register', async (req, res)=>{
                 const dataToSave = await user.save();
                 return res.status(200).json({status: "Bem-vindo adm"});  
             }
-            var docs = [];
-            const keysDB = await Key.find({},{lockId: 1, _id: 0});
-            for(i=0; i<keysDB.length; i++){
-                for(j=0; j<req.body.key.length; j++){
-                    if(JSON.stringify(keysDB[i]).split('"')[3] == req.body.key[j].id){
-                        const relation = new TeacherKey({
-                            lockFID: req.body.key[j].id,
-                            teacherName: req.body.name,
-                            schedule: [
-                                req.body.key[j].time.min, 
-                                req.body.key[j].time.max
-                            ],
-                            accepted: false
-                        });
-                        docs.push(relation);
+            else{
+                var docs = [];
+                const keysDB = await Key.find({},{lockId: 1, _id: 0});
+                for(i=0; i<keysDB.length; i++){
+                    for(j=0; j<req.body.key.length; j++){
+                        if(JSON.stringify(keysDB[i]).split('"')[3] == req.body.key[j].id){
+                            const relation = new TeacherKey({
+                                lockFID: req.body.key[j].id,
+                                teacherName: req.body.name,
+                                schedule: [
+                                    req.body.key[j].time.min, 
+                                    req.body.key[j].time.max
+                                ],
+                                accepted: false
+                            });
+                            docs.push(relation);
+                        }
                     }
                 }
+                const keySet = await TeacherKey.insertMany(docs);
+                if(keySet) {
+                    return res.status(400).json({})
+                }
+                const user = new Teacher({
+                    name: name,
+                    password: req.body.password
+                    });
+                const dataToSave = await user.save()
+                return res.status(200).json({status: "Cadastrado com sucesso"});
             }
-            const keySet = await TeacherKey.insertMany(docs);
-            if(keySet) {
-                return res.status(400).json({})
-            }
-            const user = new Teacher({
-                name: name,
-                password: req.body.password
-                });
-            const dataToSave = await user.save()
-            return res.status(200).json({status: "Cadastrado com sucesso"});
         }
         else return res.status(406).json("Docente já cadastrado");
     }
@@ -78,8 +126,12 @@ router.post('/unlockDoor', async (req,res)=>{
         for(i=0;i<user.length;i++){
             console.log(user[i].lockFID);
             if(req.body.lockId == user[i].lockFID){
-                flag = true;
-                break;
+                const hour = new Date().toLocaleTimeString('en-GB', { hour: "numeric"});
+                if(hour>=req.body.inicio&&hour<req.body.fim){
+                    flag = true;
+                    break;
+                }
+                else return res.status(403).json({message:"Hora inapropriada"});
             }
         }
         if(flag) return res.status(200).json({message: "Sala aberta", lockId: req.body.lockId, professor: req.body.name});
@@ -111,28 +163,28 @@ router.get('/request', async (req,res)=>{
 router.post('/salas', async (req,res)=>{
     var docs = []
     var sala;
-    var aux = 0;
+    var aux = 1;
     for(i=101; i<121; i++){
-        sala = new Key({lockId:"A"+i})
+        sala = new Key({lockId:"A"+i, ID:aux})
         docs.push(sala);
         aux++;
     }   
     for(i=201; i<211; i++){
-        sala = new Key({lockId:"A"+i})
+        sala = new Key({lockId:"A"+i, ID:aux})
         docs.push(sala);
         aux++;
     }
     for(i=301; i<321; i++){
-        sala = new Key({lockId:"A"+i})
+        sala = new Key({lockId:"A"+i, ID:aux})
         docs.push(sala);
         aux++;
     }
     for(i=101; i<121; i++){
-        sala = new Key({lockId:"C"+i})
+        sala = new Key({lockId:"C   "+i, ID:aux})
         docs.push(sala);
         aux++;
     }
-    const salas = await Key.insertMany(docs);
+    // const salas = await Key.insertMany(docs);
     return res.status(200).json("Salas cadastradas");
 })
 
--- back/utils/utils.js
+++ back/utils/utils.js
@@ -9,5 +9,22 @@ module.exports = {
         }
         const token = jwt.sign(payload, process.env.SECRET, {"algorithm":"HS256"});
         return token;
+    },
+    async dealLock(name){
+        const coisas = await TeacherKey.find({teacherName: name}, {lockFID: 1, _id: 0, schedule: 1});
+        console.log(JSON.stringify(coisas[0]).split('"')[7]);
+        console.log(JSON.stringify(coisas[0]).split('"')[9]);
+        var salas = []
+        for(var i=0;i<coisas.length;i++){
+            const aux = JSON.stringify(coisas[i]).split('"');
+            const sala = 
+                "lockID|"+
+                aux[3]+
+                "|schedule|"+
+                aux[7].split(":")[0]+"|"+
+                aux[9].split(":")[0];
+            salas.push(sala);
+        }
+        return salas;
     }
 }
